49c21e892968ea6b1eb9497a7b9b1f474e930977
nevaslon0117@gmail.com <nevaslon0117@gmail.com>
2023-05-18 23:16:35 -0700

    FinalCode

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
+const util = require('util');
+const { default: Prompt } = require('inquirer/lib/prompts/base');
 // TODO: Create an array of questions for user input
-const questions = [];
-inquirer
-.prompt([
+const questions = [
+
     {
         type:"input",
         message:"What is the name of the project?",
-        name: "ProjectName",
+        name: "Title",
     },
     {
         type:"input",
@@ -17,21 +19,68 @@ inquirer
     },
     {
         type:"input",
-        message:"What instructions do you want to assist users with your project?",
+        message:"Provide a table of contents",
+        name:"TableofContents",
+    },
+    {
+        type:"input",
+        message:"What instructions do you have  to assist users with your project?",
         name:"Usage",
     },
     {
         type:"input",
-        message:"",
+        message:"List any collaborators and any third party assets",
+        name:"Credits"
+    },
+    {
+        type:"checkbox",
+        message:"Choose which technologies were used in this project",
+        choices:["HTML","CSS","Javascript","Node.js"],
+        name:"TechUsed",
+
+
+    },
+    {
+        type:"input",
+        message:"What are your steps for installation?",
+        name:"Installation"
+    },
+    {
+        type:"list",
+        message:"Choose a license for your project",
+        choices:["MIT","Academic Free License v3.0", "Apache license 2.0","Microsoft Public License" ],
         name:"License",
-    }
-])
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, generateMarkdown(data), err => {
+        if (err) {
+            return console.log(err);
+        }
+        console.log("The Job was successful!")
+    });
+}
+
 
 // TODO: Create a function to initialize app
-function init() {}
+ function init() {
+    
+        inquirer
+            .prompt(questions)
+                .then(response => {
+                    writeToFile('NewREADME.md', response)
+                });
+    
+            }
+        
+    
+
+
+
+
+
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,81 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(License) {
+  let badge;
+
+  switch (License) {
+    case "MIT":
+      badge = { name: "MIT", color: "green"};
+      break;
+    case "Academic Free License v3.0":
+      badge = { name: "Academic+Free+License+v3.0", color:"blue"};
+      break;
+    case "Apache license 2.0":
+      badge = { name: "Apache+License+2.0", color:"orange"};
+      break;
+    case "Microsoft Public License":
+      badge = { name: "Microsoft+Public+License", color:"purple"};
+      break;
+
+
+  }
+  return `https://img.shields.io/static/v1?label=license&message=${badge.License}&color=${badge.License})`;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(License) {
+  let link;
+
+  switch (License) {
+    case "MIT":
+      link = "mit";
+      break;
+    case "Academic Free License v3.0":
+      link = "afl-3.0";
+      break;
+    case "Apache License 2.0":
+      link ="apache-2.0";
+      break;
+    case "Microsoft Public License":
+      link = "ms-pl";
+      break;
+  }
+  return `https://choosealicense.com/licenses/${link}/`;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// function renderLicenseSection(license) {}
 
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
 
-`;
+function generateMarkdown(userResponse) {
+var licenseBadge = renderLicenseBadge(userResponse.License)
+  var licenselink = renderLicenseLink(userResponse.License)
+  var result = (`\n#${userResponse.Title}
+
+\n![License](https://img.shields.io/badge/license-${userResponse.License}-orange.svg) \n
+\n## Description
+\n${userResponse.Description}
+\n## Table of Contents
+\n* [Installation](#Installation)
+\n* [Usage](#Usage)
+\n* [Credits](#Credits)
+\n* [Technology](#Technology)
+\n* [License](#License)
+\n## Installation
+\n${userResponse.Installation}
+\n## Usage
+\n${userResponse.Usage}
+\n## Credits
+\n${userResponse.Credits}
+\n## Technology Used
+\n${userResponse.TechUsed}
+\n## License
+\nThis project is licensed under the ${userResponse.License} - see the [License](${licenselink}) page.`)
+
+return result ;
 }
 
-module.exports = generateMarkdown;
+
+ module.exports = generateMarkdown;
